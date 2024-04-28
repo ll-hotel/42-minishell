2499725e62c1c7d1a31f4d65a332aecb92de82be
luc20131 <lucasrichaud02@gmail.com>
2024-04-28 08:15:49 +0200

    problem rewrite on the same line solved

--- src/display_prompt.c
+++ src/display_prompt.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   display_prompt.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ll-hotel <ll-hotel@student.42lyon.fr>      +#+  +:+       +#+        */
+/*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 19:17:03 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/04/17 19:19:07 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/04/28 08:13:42 by lrichaud         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ char	*display_prompt(void)
 {
 	char	*input;
 
-	input = readline("[\e[96m\e[1mminiChell\e[0m\e[39m]-$ ");
+	input = readline("[\001\e[96m\e[1m\002miniChell\001\e[0m\e[39m\002]-$ ");
 	if (input && !is_only_space(input))
 	{
 		add_history(input);
