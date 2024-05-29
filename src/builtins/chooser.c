6d79780ecabee7773be137ac570c134e6d57851b
ll-hotel <ll-hotel@student.42lyon.fr>
2024-05-29 07:00:05 +0200

    Repaired badly merged chooser.c

--- src/builtins/chooser.c
+++ src/builtins/chooser.c
@@ -6,52 +6,15 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/04 11:10:45 by lrichaud          #+#    #+#             */
-/*   Updated: 2024/05/29 06:51:41 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/05/29 06:58:06 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static int	is_not_builtin(char *command);
-
-int	ft_is_number(char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[0] == '\0')
-			return (0);
-		if (!ft_isdigit(str[i]))
-		{
-			if (str[i] != '-' && str[i] != '+')
-				return (0);
-			if (str[i + 1] == '\0')
-				return (0);
-			if (i >= 1 && str[i - 1] != ' ')
-				return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
-void	exit_error_checker(t_msh *msh, t_command *cmd)
-{
-	if (cmd->argc > 2)
-	{
-		write(2, " too many arguments\n", 20);
-		msh_exit(msh, 1);
-	}
-	if (!ft_is_number(cmd->argv[1]))
-	{
-		write(2, " numeric argument required\n", 27);
-		msh_exit(msh, 2);
-		return ;
-	}
-	msh_exit(msh, ft_atoi(cmd->argv[1]));
-}
+static int	ft_is_number(char *str);
+static void	exit_error_checker(t_msh *msh, t_command *cmd);
 
 int	chooser(t_command *cmd, t_msh *msh)
 {
@@ -74,7 +37,7 @@ int	chooser(t_command *cmd, t_msh *msh)
 	if (!ft_strncmp(cmd->argv[0], "unset", 6))
 		return (msh_unset(cmd, msh));
 	if (!ft_strncmp(cmd->argv[0], "exit", 5))
-		msh_exit(msh, ft_atoi(cmd->argv[1]));
+		exit_error_checker(msh, cmd);
 	return (-1);
 }
 
@@ -87,4 +50,43 @@ static int	is_not_builtin(char *command)
 			ft_strncmp(command, "export", 7) && \
 			ft_strncmp(command, "unset", 6) && \
 			ft_strncmp(command, "exit", 5));
+}
+
+static int	ft_is_number(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[0] == '\0')
+			return (0);
+		if (!ft_isdigit(str[i]))
+		{
+			if (str[i] != '-' && str[i] != '+')
+				return (0);
+			if (str[i + 1] == '\0')
+				return (0);
+			if (i >= 1 && str[i - 1] != ' ')
+				return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+static void	exit_error_checker(t_msh *msh, t_command *cmd)
+{
+	if (cmd->argc > 2)
+	{
+		write(2, " too many arguments\n", 20);
+		msh_exit(msh, 1);
+	}
+	if (!ft_is_number(cmd->argv[1]))
+	{
+		write(2, " numeric argument required\n", 27);
+		msh_exit(msh, 2);
+		return ;
+	}
+	msh_exit(msh, ft_atoi(cmd->argv[1]));
 }
