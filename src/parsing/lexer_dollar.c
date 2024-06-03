6ee9ccc0d246f44961453ffafa8791265abe3554
ll-hotel <150347935+ll-hotel@users.noreply.github.com>
2024-06-03 03:44:47 +0200

    ~ handling (#52)

--- src/parsing/lexer_dollar.c
+++ src/parsing/lexer_dollar.c
@@ -6,7 +6,7 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 05:09:41 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/05/29 06:18:30 by lrichaud         ###   ########lyon.fr   */
+/*   Updated: 2024/06/03 03:40:00 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,20 @@ t_token	*lexer_dollar(char *line, int *p_i)
 	int		word_len;
 
 	word_len = 1;
-	if (line[1] == '?')
-		word = ft_substr(line, 1, word_len++);
+	if (line[0] == '~')
+		word = ft_strdup("HOME");
 	else
 	{
-		while (line[word_len] \
-			&& !ft_isblank(line[word_len]) \
-			&& !is_operator(line[word_len]))
-			word_len += 1;
-		word = ft_substr(line, 1, word_len - 1);
+		if (line[1] == '?')
+			word = ft_substr(line, 1, word_len++);
+		else
+		{
+			while (line[word_len] \
+					&& !ft_isblank(line[word_len]) \
+					&& !is_operator(line[word_len]))
+				word_len += 1;
+			word = ft_substr(line, 1, word_len - 1);
+		}
 	}
 	*p_i = word_len;
 	if (word)
