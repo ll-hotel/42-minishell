2a55ed5f00dfd65275bc9d27334655030070ea71
ll-hotel <ll-hotel@student.42lyon.fr>
2024-06-03 19:42:45 +0200

    Fixed?

--- src/exec/exec_pipeline.c
+++ src/exec/exec_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: ll-hotel <ll-hotel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 13:28:45 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/06/03 06:46:41 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/03 19:41:31 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ int	exec_pipeline(t_msh *msh, t_command *cmd)
 			exec_perror_exit(msh, errno);
 		else if (pid == 0)
 			mini_chooser(msh, cmd);
+		close(fd_pipe[1]);
+		ft_close(cmd->fd_in);
 		cmd = cmd->next;
 	}
 	return (exec_wait_children());
@@ -50,6 +52,10 @@ static void	mini_chooser(t_msh *msh, t_command *cmd)
 	if (chooser(cmd, NULL) == 0)
 		status = chooser(cmd, msh);
 	else
+	{
+		if (cmd->next)
+			cmd->next->fd_in = ft_close(cmd->next->fd_in);
 		exec_one(msh, cmd);
+	}
 	msh_exit(msh, status);
 }
