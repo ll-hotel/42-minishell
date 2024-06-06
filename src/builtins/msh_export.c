c83e08f2bef773350dc5a596a932b0abe9d2ae97
ll-hotel <ll-hotel@student.42lyon.fr>
2024-06-06 15:22:54 +0200

    Export errors fixes (still not able to export multiple args)

--- src/builtins/msh_export.c
+++ src/builtins/msh_export.c
@@ -6,7 +6,7 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 06:51:48 by lrichaud          #+#    #+#             */
-/*   Updated: 2024/06/03 11:32:53 by lrichaud         ###   ########lyon.fr   */
+/*   Updated: 2024/06/06 15:21:32 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ int	msh_export(t_command *cmd, t_msh *msh)
 	t_env_var	*new_env_var;
 	t_env_var	*vars;
 
+	new_env_var = env_var_new(cmd->argv[1]);
+	if (!new_env_var)
+		return (perror("export"), 1);
+	if (!env_var_is_valid(new_env_var->name))
+		return (1);
 	if (ft_strchr(cmd->argv[1], '='))
 	{
-		new_env_var = env_var_new(cmd->argv[1]);
-		if (!new_env_var)
-			return (1);
 		vars = (t_env_var *)msh->env_vars.first;
 		while (vars && ft_strncmp(vars->name, new_env_var->name, \
 			ft_strlen(new_env_var->name)))
 			vars = vars->next;
-		if (!env_var_is_valid(new_env_var->name))
-			return (1);
 		if (!vars)
 			llst_addback(&msh->env_vars, (t_llst *)new_env_var);
 		else if (new_env_var->value)
@@ -49,12 +49,15 @@ static int	env_var_is_valid(char *arg)
 
 	i = 1;
 	if (!ft_isalpha(arg[0]))
+	{
+		ft_dprintf(2, "export: %s: not a valid identifier\n", arg);
 		return (0);
+	}
 	while (ft_isalnum(arg[i]))
 		i++;
 	if (arg[i])
 	{
-		write(2, "export : not a valid identifier\n", 32);
+		ft_dprintf(2, "export: %s: not a valid identifier\n", arg);
 		return (0);
 	}
 	return (1);
