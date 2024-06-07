87e0f05b3dfd551b838d7f0efdf64a5ab9c18b7d
ll-hotel <ll-hotel@student.42lyon.fr>
2024-06-07 21:55:45 +0200

    Fix: SHLVL was not incrementing

--- src/core/env.c
+++ src/core/env.c
@@ -6,11 +6,13 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 22:26:59 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/05/29 02:51:41 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/07 21:54:53 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*get_shlvl(char *old_value);
 
 int	env_init(t_msh *msh, char *const *envp)
 {
@@ -27,6 +29,23 @@ int	env_init(t_msh *msh, char *const *envp)
 			return (0);
 		}
 		llst_addback(&msh->env_vars, (t_llst *)var);
+		if (ft_strncmp(var->name, "SHLVL", 6) == 0)
+			var->value = get_shlvl(var->value);
 	}
 	return (1);
+}
+
+static char	*get_shlvl(char *old_value)
+{
+	char	*tmp;
+
+	if (!old_value || !ft_is_number(old_value))
+	{
+		tmp = ft_strdup("1");
+	}
+	else
+		tmp = ft_itoa(ft_atoi(old_value) + 1);
+	if (old_value)
+		free(old_value);
+	return (tmp);
 }
