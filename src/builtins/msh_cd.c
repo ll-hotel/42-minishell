daa6739170f9564cdb9f81d3009581c42d6e4c32
luc20131 <lucasrichaud02@gmail.com>
2024-06-10 01:52:33 +0200

    CD change les Vars PWD et OLDPWD, il faut juste normed et gerer les leaks

--- src/builtins/msh_cd.c
+++ src/builtins/msh_cd.c
@@ -16,13 +16,16 @@ int	msh_cd(t_command *cmd, t_msh *env)
 {
 	char		*new_path;
 	t_env_var	*home;
+	t_env_var	*FF;
 	int			test;
+	char		*old_path;
 
 	if (cmd->argc > 2)
 	{
 		write(2, "miniChell: too many arguments\n", 30);
 		return (1);
 	}
+	old_path = getcwd(NULL, 0);
 	if (cmd->argc == 1)
 	{
 		home = env_var_get(env, "HOME");
@@ -31,23 +34,29 @@ int	msh_cd(t_command *cmd, t_msh *env)
 			write(2, "cd: HOME not set\n", 18);
 			return (1);
 		}
-		return (0);
 	}
-	new_path = cmd->argv[1];
-	test = chdir(new_path);
-	if (test == -1)
+	else
 	{
-		perror("cd");
-		return (1);
+		new_path = cmd->argv[1];
+		test = chdir(new_path);
+		if (test == -1)
+		{
+			perror("cd");
+			return (1);
+		}
 	}
+	FF = (t_env_var *) env->env_vars.first;
+	while (ft_strncmp("OLDPWD",FF->name, 7))
+	{
+		FF = FF->next;
+	}
+	FF->value = strdup(old_path);
+	FF = (t_env_var *) env->env_vars.first;
+	while (ft_strncmp("PWD",FF->name, 4))
+	{
+		FF = FF->next;
+	}
+	FF->value = strdup(getcwd(NULL,0));
 	return (0);
 }
 
-void	refresh_pwd(void)
-{
-	t_command	truc;
-
-	truc.argv = ft_calloc(3, sizeof(char *));
-	truc.argv[1] = ft_strjoin("OLDPWD=", getcwd(NULL, 0));
-	truc.argv[2] = NULL;
-}
