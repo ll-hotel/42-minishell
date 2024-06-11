e9fae24eb3191c0f6fefc2d1273919a40a5b1b18
luc20131 <lucasrichaud02@gmail.com>
2024-06-11 20:06:09 +0200

    j'ai change le joint

--- src/builtins/msh_cd.c
+++ src/builtins/msh_cd.c
@@ -12,11 +12,12 @@
 
 #include "minishell.h"
 
+int	set_evar_value(t_msh *env, char *name, char *value);
+
 int	msh_cd(t_command *cmd, t_msh *env)
 {
 	char		*new_path;
 	t_env_var	*home;
-	t_env_var	*FF;
 	int			test;
 	char		*old_path;
 
@@ -26,12 +27,18 @@ int	msh_cd(t_command *cmd, t_msh *env)
 		return (1);
 	}
 	old_path = getcwd(NULL, 0);
+	if (old_path == NULL)
+	{
+		perror("cd");
+		return (1);
+	}
 	if (cmd->argc == 1)
 	{
 		home = env_var_get(env, "HOME");
-		if (!home || chdir(home->value) == -1)
+		if (home == NULL || chdir(home->value) == -1)
 		{
 			write(2, "cd: HOME not set\n", 18);
+			ft_free(old_path);
 			return (1);
 		}
 	}
@@ -41,22 +48,52 @@ int	msh_cd(t_command *cmd, t_msh *env)
 		test = chdir(new_path);
 		if (test == -1)
 		{
+			ft_free(old_path);
 			perror("cd");
 			return (1);
 		}
 	}
-	FF = (t_env_var *) env->env_vars.first;
-	while (ft_strncmp("OLDPWD",FF->name, 7))
-	{
-		FF = FF->next;
-	}
-	FF->value = strdup(old_path);
-	FF = (t_env_var *) env->env_vars.first;
-	while (ft_strncmp("PWD",FF->name, 4))
-	{
-		FF = FF->next;
-	}
-	FF->value = strdup(getcwd(NULL,0));
+	set_evar_value(env, "OLDPWD", old_path);
+	new_path = getcwd(NULL, 0);
+	set_evar_value(env, "PWD", new_path);
+	free(old_path);
+	free(new_path);
 	return (0);
 }
 
+char	*free_to_join(char *str1, char *str2)
+{
+	char	*temp;
+	temp = ft_strdup(str1);
+	if (temp == NULL)
+		return (NULL);
+	free(str1);
+	str1 = ft_strjoin(temp, str2);
+	free(temp);
+	return (str1);
+}
+
+int	set_evar_value(t_msh *env, char *name, char *value)
+{
+	t_env_var	*evar;
+
+	evar = env_var_get(env, name);
+	if (evar == NULL)
+	{
+		name = free_to_join(name, "=");
+		if (name == NULL)
+		{
+			perror("cd");
+			return (1);
+		}
+		evar = env_var_new(name);
+	}
+	ft_free(evar->value);
+	evar->value = ft_strdup(value);
+	if (evar->value == NULL)
+	{
+		perror("cd");
+		return (1);
+	}
+	return (0);
+}
