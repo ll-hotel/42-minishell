dd7e34563ad6c1953b8a4493333ef7dbca10d891
luc20131 <lucasrichaud02@gmail.com>
2024-06-13 07:08:58 +0200

    cd normed et refresh PWD and OLDPWD

--- src/builtins/msh_cd.c
+++ src/builtins/msh_cd.c
@@ -12,34 +12,40 @@
 
 #include "minishell.h"
 
-int	set_evar_value(t_msh *env, char *name, char *value);
+int	set_evar_value(t_msh *msh, char *name, char *value);
+int	refresh_pwd(t_msh *env, char *old_path, char *new_path);
+int	chdir_cd(t_msh *msh, t_command *cmd, char *old_path, char *new_path);
 
-int	msh_cd(t_command *cmd, t_msh *env)
+int	msh_cd(t_command *cmd, t_msh *msh)
 {
 	char		*new_path;
-	t_env_var	*home;
-	int			test;
 	char		*old_path;
 
+	new_path = NULL;
 	if (cmd->argc > 2)
-	{
-		write(2, "miniChell: too many arguments\n", 30);
-		return (1);
-	}
+		return (write(2, "miniChell: too many arguments\n", 30), 1);
 	old_path = getcwd(NULL, 0);
 	if (old_path == NULL)
-	{
-		perror("cd");
+		return (perror("cd"), 1);
+	if (chdir_cd(msh, cmd, old_path, new_path))
 		return (1);
-	}
+	if (refresh_pwd(msh, old_path, new_path))
+		return (1);
+	return (0);
+}
+
+int	chdir_cd(t_msh *msh, t_command *cmd, char *old_path, char *new_path)
+{
+	t_env_var	*home;
+	int			test;
+
 	if (cmd->argc == 1)
 	{
-		home = env_var_get(env, "HOME");
+		home = env_var_get(msh, "HOME");
 		if (home == NULL || chdir(home->value) == -1)
 		{
-			write(2, "cd: HOME not set\n", 18);
 			ft_free(old_path);
-			return (1);
+			return (write(2, "cd: HOME not set\n", 18), 1);
 		}
 	}
 	else
@@ -49,13 +55,21 @@ int	msh_cd(t_command *cmd, t_msh *env)
 		if (test == -1)
 		{
 			ft_free(old_path);
-			perror("cd");
-			return (1);
+			return (perror("cd"), 1);
 		}
 	}
-	set_evar_value(env, "OLDPWD", old_path);
+	return (0);
+}
+
+int	refresh_pwd(t_msh *env, char *old_path, char *new_path)
+{
+	if (set_evar_value(env, "OLDPWD", old_path))
+		return (1);
 	new_path = getcwd(NULL, 0);
-	set_evar_value(env, "PWD", new_path);
+	if (new_path == NULL)
+		return (1);
+	if (set_evar_value(env, "PWD", new_path))
+		return (1);
 	free(old_path);
 	free(new_path);
 	return (0);
@@ -64,6 +78,7 @@ int	msh_cd(t_command *cmd, t_msh *env)
 char	*free_to_join(char *str1, char *str2)
 {
 	char	*temp;
+
 	temp = ft_strdup(str1);
 	if (temp == NULL)
 		return (NULL);
@@ -73,11 +88,11 @@ char	*free_to_join(char *str1, char *str2)
 	return (str1);
 }
 
-int	set_evar_value(t_msh *env, char *name, char *value)
+int	set_evar_value(t_msh *msh, char *name, char *value)
 {
 	t_env_var	*evar;
 
-	evar = env_var_get(env, name);
+	evar = env_var_get(msh, name);
 	if (evar == NULL)
 	{
 		name = free_to_join(name, "=");
@@ -87,6 +102,8 @@ int	set_evar_value(t_msh *env, char *name, char *value)
 			return (1);
 		}
 		evar = env_var_new(name);
+		if (evar == NULL)
+			return (0);
 	}
 	ft_free(evar->value);
 	evar->value = ft_strdup(value);
