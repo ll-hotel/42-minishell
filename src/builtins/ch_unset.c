cdd1ffeb3cb026294f291b243d498d4e46a809c1
Lucas Richaud <lrichaud@z3r11p3.42lyon.fr>
2024-06-20 02:30:34 +0200

    unset repared je kroa

--- src/builtins/ch_unset.c
+++ src/builtins/ch_unset.c
@@ -6,7 +6,7 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 06:53:03 by lrichaud          #+#    #+#             */
-/*   Updated: 2024/06/17 22:04:13 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/20 02:14:21 by lrichaud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ int	ch_unset(t_cmd *cmd, t_ch *ch)
 	if (cmd->argc < 2)
 		return (0);
 	vars = (t_evar *)ch->evars.first;
-	while (vars && ft_strncmp(vars->name, cmd->argv[1], \
-		ft_strlen(cmd->argv[1])))
+	while (vars && ft_strcmp(vars->name, cmd->argv[1]))
 	{
 		previous_vars = (t_llst_head *) previous_vars->first;
 		vars = vars->next;
