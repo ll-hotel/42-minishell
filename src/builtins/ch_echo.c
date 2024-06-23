05b36a96cd9ea6dd061a6a6039b9dcf7305d57ae
ll-hotel <150347935+ll-hotel@users.noreply.github.com>
2024-06-23 20:58:44 +0200

    Fixed echo not printing (#77)

--- src/builtins/ch_echo.c
+++ src/builtins/ch_echo.c
@@ -6,7 +6,7 @@
 /*   By: lrichaud <lrichaud@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/26 20:38:07 by lrichaud          #+#    #+#             */
-/*   Updated: 2024/05/23 16:34:51 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/23 20:48:54 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,24 @@ static int	is_param(char *str);
 
 int	ch_echo(t_cmd *cmd)
 {
-	char	backslash;
+	char	backslash[2];
 	int		i;
 
-	backslash = '\n';
+	backslash[0] = '\n';
+	backslash[1] = 0;
 	i = 1;
 	while (i < cmd->argc && is_param(cmd->argv[i]))
 		i += 1;
 	if (i > 1)
-		backslash = 0;
+		*backslash = 0;
 	while (i < cmd->argc)
 	{
-		printf("%s", cmd->argv[i]);
+		ft_putstr_fd(cmd->argv[i], 1);
 		if (i + 1 < cmd->argc)
-			printf(" ");
+			ft_putstr_fd(" ", 1);
 		i += 1;
 	}
-	printf("%c", backslash);
+	ft_putstr_fd(backslash, 1);
 	return (0);
 }
 
