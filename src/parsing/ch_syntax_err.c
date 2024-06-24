57b02204233dca4c26850dacf9492086069713ef
ll-hotel <ll-hotel@student.42lyon.fr>
2024-06-24 11:20:14 +0200

    Fix syntax error message

--- src/parsing/ch_syntax_err.c
+++ src/parsing/ch_syntax_err.c
@@ -6,7 +6,7 @@
 /*   By: ll-hotel <ll-hotel@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 04:38:12 by ll-hotel          #+#    #+#             */
-/*   Updated: 2024/06/04 21:45:55 by ll-hotel         ###   ########.fr       */
+/*   Updated: 2024/06/24 11:19:07 by ll-hotel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,10 @@ void	ch_syntax_err(char c)
 		ft_dprintf(2, \
 			"miniChell: unexpected EOF while looking for matching `%c'\n", -c);
 	}
-	if (!c || c == - '\'' || c == - '\"')
+	if (c == - '\'' || c == - '\"')
 		ft_dprintf(2, "miniChell: syntax error: unexpected end of file\n");
+	else if (!c)
+		ft_dprintf(2, "miniChell: syntax error " \
+				"near unexpected token `newline'\n");
 	ch_status_set(2);
 }
